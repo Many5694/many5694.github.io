0bf4855fbb38002c58bd789db3b7acb816477e5e
Manuel Meléndez <manuel.melendez2@upr.edu>
2017-08-22 21:55:29 -0400

    CV (many5694.design)

--- resources/Many5694/Tests/test.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-htmlreq = urllib.request.urlopen("https://3dr.com/faa/drone-practice-tests/")
-htmltext = htmlreq.read()
-print(str(htmltext,'utf-8'))
-input("Pause for view text-------------------------------------------------")
--- resources/Many5694/Tests/test2.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-i=1
-while (i < 27):
-    htmlreq = urllib.request.urlopen("https://3dr.com/faa/drone-practice-tests/"+"i")
-    htmltext = htmlreq.read()
-    print(str(htmltext,'utf-8'))
-i += 1
-
-input("Pause for view text-------------------------------------------------")
